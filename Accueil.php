7e83295fe79313ccaff7386605af8ab7364a58a0
OpsiteLV <lucasveliabtssio@gmail.com>
2023-04-24 08:05:46 +0400

    First commit

--- Accueil.php
+++ Accueil.php
@@ -1,2 +1,22 @@
-<h1> Hello World Bomba !!!</h1>
-<img src="images/Placeholder.jpg">
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-8">
+      <div class="main-text">
+        <h1>Main content goes here</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel mauris in lectus rutrum fringilla. Vivamus pulvinar, odio a lacinia lacinia, nibh nibh malesuada lectus, in dapibus sapien purus nec eros. Donec vitae justo commodo, viverra risus quis, congue leo. Etiam dignissim ligula in nulla bibendum, quis viverra purus vestibulum. </p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="sidebar">
+        <h2>Side content goes here</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel mauris in lectus rutrum fringilla. Vivamus pulvinar, odio a lacinia lacinia, nibh nibh malesuada lectus, in dapibus sapien purus nec eros. Donec vitae justo commodo, viverra risus quis, congue leo. Etiam dignissim ligula in nulla bibendum, quis viverra purus vestibulum. </p>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ Accueil2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  width: 200px;
+  background-color: #f1f1f1;
+}
+
+li a {
+  display: block;
+  color: #000;
+  padding: 8px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color on hover */
+li a:hover {
+  background-color: #555;
+  color: white;
+}
+</style>
+</head>
+<body>
+
+<h1>Vélia Lucas</h1>
+
+<ul>
+  <li><a href="#Accueil">Accueil</a></li>         
+  <li><a href="#BTS SIO">BTS SIO</a></li>
+  <li><a href="#Stages">Stages</a></li>
+  <li><a href="#Projets">Projets</a></li>
+  <li><a href="#Veilles">Veilles</a></li>
+  <li><a href="#Curriculum Vitae">Curriculum Vitae</a></li>
+  <li><a href="#Contact">Contact</a></li>
+</ul>
+<h3> Bienvenue sur mon Portfolio</h3>
+</body>
+</html>
